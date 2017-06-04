c3f350d9a0d6318cdeb561201339b1b56bc5ed56
henrique elias <hcbe2004@gmail.com>
2017-06-04 15:51:18 -0300

    facebook button

---
+++ client/components/facebook-button/facebook-button.component.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function() {
+  class FacebookButtonController {
+
+    constructor($window) {
+      this.window = $window;
+
+    }
+    openFacebook() {
+      this.window.open("https://www.facebook.com/advterramoreira/", "_blank");
+    }
+
+  }
+
+  angular.module('webapp')
+    .component('facebookButton', {
+      templateUrl: 'components/facebook-button/facebook-button.html',
+      controller: FacebookButtonController
+    });
+})();
