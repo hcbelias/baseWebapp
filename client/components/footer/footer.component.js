5f4b17ce8c6d5230419f5869274012e194c43d4e
henrique elias <hcbe2004@gmail.com>
2017-06-04 16:56:50 -0300

    contact changes

--- client/components/footer/footer.component.js
+++ client/components/footer/footer.component.js
@@ -7,10 +7,7 @@
         this.window = $window;
     }
 
-      openFacebook() {
-        this.window.open("https://www.facebook.com/advterramoreira/", "_blank");
-      }
-
+    
   }
 
   angular.module('webapp')
